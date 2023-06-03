4b7dad99cbf151dd7ec1e3cc9a61f9bbf812a67a
Natan Boschetti <natansb0787@gmail.com>
2023-06-03 19:56:38 -0300

    add button events to finish and remove

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const todoForm = document.querySelector("#todoForm");
 const inputTodo = document.querySelector("#inputTodo");
 const todoList = document.querySelector("#todoList");
+const todoContainer = document.querySelector("#todoContainer");
 
+let todoContainerElement;
 function saveTodoInContainer(todoText) {
-  console.log(`salvo: ${todoText}`);
+  //console.log(`salvo: ${todoText}`);
 
-  const todoContainerElement = document.createElement("div");
+  todoContainerElement = document.createElement("div");
   todoList.appendChild(todoContainerElement);
 
   const todoTextElement = document.createElement("p");
@@ -13,14 +15,16 @@ function saveTodoInContainer(todoText) {
   todoContainerElement.appendChild(todoTextElement);
 
   const buttonFinishTodoElement = document.createElement("button");
-  buttonFinishTodoElement.innerHTML = '<i class="bi bi-check-lg">';
+  buttonFinishTodoElement.classList.add("finish-todo");
+  buttonFinishTodoElement.innerHTML = '<i class="bi bi-check-lg finish-todo">';
   todoContainerElement.appendChild(buttonFinishTodoElement);
 
   const buttonRemoveTodoElement = document.createElement("button");
-  buttonRemoveTodoElement.innerHTML = '<i class="bi bi-x-lg">';
+  buttonRemoveTodoElement.classList.add("remove-todo");
+  buttonRemoveTodoElement.innerHTML = '<i class="bi bi-x-lg remove-todo">';
   todoContainerElement.appendChild(buttonRemoveTodoElement);
 
-  console.log(`Container: ${todoList.innerHTML}`);
+  //console.log(`Container: ${todoList.innerHTML}`);
 }
 
 todoForm.addEventListener("submit", (e) => {
@@ -28,9 +32,28 @@ todoForm.addEventListener("submit", (e) => {
 
   if (inputTodo.value) {
     saveTodoInContainer(inputTodo.value);
-    console.log(inputTodo.value);
+    //console.log(inputTodo.value);
   }
 
   inputTodo.value = "";
   inputTodo.focus();
+});
+
+document.addEventListener("click", (e) => {
+  const targetElement = e.target;
+  const targetElementParent = targetElement.closest("div");
+  //console.log(targetElement);
+
+  const containsFinishClass = targetElement.classList.contains("finish-todo");
+  const containsRemoveClass = targetElement.classList.contains("remove-todo");
+  if (containsFinishClass) {
+    //console.log(todoContainerElement);
+    todoContainerElement.classList.toggle("todo-done");
+    console.log("finish", containsFinishClass);
+  }
+  if (containsRemoveClass) {
+    console.log(targetElementParent);
+    targetElementParent.remove();
+    console.log("remove", containsRemoveClass);
+  }
 });
